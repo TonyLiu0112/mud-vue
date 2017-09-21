adb9335941d5f8261d165e8e962cc50fec6e194a
tonyLiu0112 <tony.liu0112@gmail.com>
2017-09-21 14:57:12 +0800

    实战，基础业务功能

--- src/pages/security/login.vue
+++ src/pages/security/login.vue
@@ -47,6 +47,10 @@
         }
         const ops = {
           method: 'post',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
           body: JSON.stringify(this.loginReq)
         }
         fetch(_content.API_PATH.URI + 'security/tokens', ops)
